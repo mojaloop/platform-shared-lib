aa1788949976e0b01bc52f11a9dbfd2a6f274537
Pedro Sousa Barreto <pedrosousabarreto@gmail.com>
2023-01-18 22:02:27 +0000

    Initial versions of transfers prepare/prepared events

---
+++ modules/public-messages-lib/src/transfers-bc/responses.ts
@@ -0,0 +1,65 @@
+/*****
+ License
+ --------------
+ Copyright © 2017 Bill & Melinda Gates Foundation
+ The Mojaloop files are made available by the Bill & Melinda Gates Foundation under the Apache License, Version 2.0 (the "License") and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+ Contributors
+ --------------
+ This is the official list (alphabetical ordering) of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ * Crosslake
+ - Pedro Sousa Barreto <pedrob@crosslaketech.com>
+
+ --------------
+ ******/
+
+"use strict";
+
+import { DomainEventMsg } from "@mojaloop/platform-shared-lib-messaging-types-lib";
+import {AGGREGATE_NAME, BOUNDED_CONTEXT_NAME, TransfersBCTopics} from ".";
+
+
+export type TransferPreparedEvtPayload = {
+	transferId: string;
+	amount: string;
+	currency: string;
+	payerId: string;
+	payeeId: string;
+	expirationTimestamp: number;
+	condition: string;
+}
+
+export class TransferPreparedEvt extends DomainEventMsg {
+	boundedContextName: string = BOUNDED_CONTEXT_NAME
+	aggregateId: string;
+	aggregateName: string = AGGREGATE_NAME;
+	msgKey: string;
+	msgTopic: string = TransfersBCTopics.DomainEvents;
+	payload: TransferPreparedEvtPayload;
+
+	constructor(payload: TransferPreparedEvtPayload) {
+		super();
+
+		this.aggregateId = this.msgKey = payload.transferId;
+		this.payload = payload;
+	}
+
+	validatePayload(): void {
+		// TODO
+	}
+}
