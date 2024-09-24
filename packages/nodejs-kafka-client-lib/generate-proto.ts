e865c128f2b7a51dce491a02e0fa6376f939245d
Rui Rocha <rui.rocha.work@outlook.com>
2024-09-24 17:08:22 +0100

    replaced execSync with execFileSync to fix codeql issue

--- packages/nodejs-kafka-client-lib/generate-proto.ts
+++ packages/nodejs-kafka-client-lib/generate-proto.ts
@@ -1,16 +1,19 @@
-const { execSync } = require('child_process');
+const { execFileSync } = require('child_process');
 const path = require('path');
 
 const protoDir = path.resolve(__dirname, 'src/protobuff');
 const protoFile = path.resolve(protoDir, 'messages.proto');
 const outDir = path.resolve(protoDir);
 
-const command = `protoc --ts_out=service=true:${outDir} --proto_path=${protoDir} ${protoFile}`;
-
-console.log('Running command:', command);
+const command = 'protoc';
+const args = [
+    `--ts_out=service=true:${outDir}`,
+    `--proto_path=${protoDir}`,
+    protoFile
+];
 
 try {
-    execSync(command, { stdio: 'inherit' });
+    execFileSync(command, args, { stdio: 'inherit' });
 } catch (error) {
     console.error('Error executing command:', error);
     process.exit(1);
